9019df5668c77817000a8269acbf3a613c3537bc
SirAricost <coffee-media@users.noreply.github.com>
2018-06-13 10:17:29 -0400

    Create db.js

---
+++ db.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD2wo1j1uioAU45vqNadsXTK9aSCiFN-SA",
+    authDomain: "chat-45b41.firebaseapp.com",
+    databaseURL: "https://chat-45b41.firebaseio.com",
+    projectId: "chat-45b41",
+    storageBucket: "chat-45b41.appspot.com",
+    messagingSenderId: "321201359894"
+  };
+  firebase.initializeApp(config);
